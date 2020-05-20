db3dd1552f5224f91a2cd33a0870167f95d509cc
nonda <nonda95@gmail.com>
2020-05-20 21:14:42 +0200

    Featured item UI

--- app/src/main/java/com/example/androidworkshop3/ui/main/ItemAdapter.kt
+++ app/src/main/java/com/example/androidworkshop3/ui/main/ItemAdapter.kt
@@ -10,15 +10,23 @@ import kotlinx.android.synthetic.main.item.*
 
 class ItemAdapter : RecyclerView.Adapter<ItemViewHolder>() {
     var items = emptyList<Item>()
-    set(value) {
-        field = value
-        notifyDataSetChanged()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (items[position].isFeatured) R.layout.featured_item else R.layout.item
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val itemView = layoutInflater.inflate(R.layout.item, parent, false)
-        return ItemViewHolder(itemView)
+        val itemView = layoutInflater.inflate(viewType, parent, false)
+        return when (viewType) {
+            R.layout.item -> ItemViewHolder.Default(itemView)
+            R.layout.featured_item -> ItemViewHolder.Featured(itemView)
+            else -> error("Unknown view type: $viewType")
+        }
     }
 
     override fun getItemCount() = items.size
@@ -28,12 +36,22 @@ class ItemAdapter : RecyclerView.Adapter<ItemViewHolder>() {
     }
 }
 
-
-class ItemViewHolder(
+sealed class ItemViewHolder(
     override val containerView: View
 ) : RecyclerView.ViewHolder(containerView), LayoutContainer {
-    fun bind(item: Item) {
-        primaryText.text = item.primaryText
-        secondaryText.text = item.secondaryText
+    abstract fun bind(item: Item)
+
+    class Default(containerView: View) : ItemViewHolder(containerView) {
+        override fun bind(item: Item) {
+            primaryText.text = item.primaryText
+            secondaryText.text = item.secondaryText
+        }
+    }
+
+    class Featured(containerView: View) : ItemViewHolder(containerView) {
+        override fun bind(item: Item) {
+            primaryText.text = item.primaryText
+            secondaryText.text = item.secondaryText
+        }
     }
 }
