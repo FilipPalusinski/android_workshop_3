19af6e95c58b67f933830a682557d1d03736c97f
nonda <nonda95@gmail.com>
2020-05-21 18:14:33 +0200

    Add ItemTouchHelper and tweak item animator

--- app/src/main/java/com/example/androidworkshop3/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/androidworkshop3/ui/main/MainFragment.kt
@@ -5,7 +5,10 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidworkshop3.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
@@ -13,6 +16,42 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     private val viewModel: MainViewModel by viewModels()
     private val itemAdapter = ItemAdapter()
+
+    private val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+        ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+        ItemTouchHelper.START or ItemTouchHelper.END
+    ) {
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ) = true
+
+        override fun onMoved(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            fromPos: Int,
+            target: RecyclerView.ViewHolder,
+            toPos: Int,
+            x: Int,
+            y: Int
+        ) {
+            val newList = itemAdapter.currentList.toMutableList()
+            val movedItem = newList[fromPos]
+            val targetItem = newList[toPos]
+            newList[fromPos] = targetItem
+            newList[toPos] = movedItem
+            itemAdapter.submitList(newList)
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val newList = itemAdapter.currentList.toMutableList().apply {
+                removeAt(viewHolder.adapterPosition)
+            }
+            itemAdapter.submitList(newList)
+        }
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -22,6 +61,8 @@ class MainFragment : Fragment(R.layout.main_fragment) {
         itemsRecyclerView.apply {
             adapter = itemAdapter
             layoutManager = LinearLayoutManager(context)
+            itemAnimator = DefaultItemAnimator().apply { supportsChangeAnimations = false }
+            ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(this)
         }
 
         viewModel.data.observe(viewLifecycleOwner) { itemAdapter.submitList(it) }
